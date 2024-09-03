6dab5aec5bacfcc8a8ba2b09c612d7a26f58592b
José Victor Gabriel <josevictor21112005@gmail.com>
2024-09-02 22:20:36 -0300

    #15 Dockerizando/Modelando banco de dados (MongoDB)

---
+++ models/models.py
@@ -0,0 +1,21 @@
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.favourite_receipts = []
+        self.is_admin = False
+
+class Receipt:
+    def __init__(self, name, preparation, ingredients, category):
+        self.name = name
+        self.preparation = preparation
+        self.ingredients = ingredients
+        self.category = category
+        self.comments = []
+        self.favourited_by = []
+
+class Comment:
+    def __init__(self, user_id, receipt_id, text):
+        self.user_id = user_id
+        self.receipt_id = receipt_id
+        self.text = text
