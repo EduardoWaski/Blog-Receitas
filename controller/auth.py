a5f436d5a04aa431f6070a8a9e6a5c3b57debeff
José Victor Gabriel <josevictor21112005@gmail.com>
2024-09-05 10:38:51 -0300

    #19 Segunda versão da autenticação de login (otimizando parte do código)

--- controller/auth.py
+++ controller/auth.py
@@ -1,40 +1,69 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for, flash
 from models.db_config import *
+from models.models import *
+from .auxiliar import *
 
 auth = Blueprint('auth', __name__)
 
 # User fictício
 users = {
-    'username': 'eduardo', 'password': '1234'
+    'username': 'eduardo', 'password': '1234',
 }
 
 @auth.route("/acesso", methods = ["GET", "POST"])
-def login():
-    if request.method == "POST":
-        username = request.form['username']
-        password = request.form['password']
+def login_signup():
 
-        # Verificação dos dados
-        if (username in users['username']) and (users['password'] == password):
-            session['username'] = username
-            return redirect(url_for('index'))
-        else:
-            flash('Nome de usuário e/ou senha incorretos!')
+    if request.method == "GET":
+        return render_template('login_signup.html')
 
+    # Se não for GET, é POST
+    # Recuperando os dados do formulário
+    inputed_username = request.form['username']
+    inputed_password = request.form['password']
+    login_option = True if "login_btn" in request.form.to_dict() else False
+
+    # Verificando se o usuário tentou fazer login ou cadastro
+    if login_option:
+
+        # Verificação dos dados (Trocando o "in" pelo "==")
+        # Essa parte deverá ser trocada depois, pois devemos analisar os usuários dentro do banco de dados
+        if (inputed_username == users['username']) and (inputed_password == users['password']):
+            session['username'] = inputed_username
+            session['password'] = inputed_password
+            return redirect(url_for('index.home_page'))
+        
+        # Retirada do "else", pois o "return" torna o "else" dispensável
+        flash('Nome de usuário e/ou senha incorretos!') 
+
+
+    # Se for cadastro
+    else:                   
+        print((inputed_username != users['username']))
+        print(inputed_password.isalnum())
+        if (inputed_username != users['username']) and (inputed_password.isalnum()): # Retirada do inputed_password.isalpha()
+            
+            # Criando um novo usuário a partir da classe
+            new_user = User(inputed_username, inputed_password).__dict__
+            users_collection.insert_one(new_user) # Método insert_one corrigido (insertOne -> insert_one)
+
+            # Não inserir dentro da sessão até o usuário fazer login
+            # session['username'] = inputed_username
+            # session['password'] = inputed_password
+            
+            flash("Usuário criado com sucesso!")
+
+        flash('Nome de usuário digitado já existe!')
+    
     return render_template('login_signup.html')
 
-def sign_up():
-    if request.method == "POST":
-        created_username = request.form['username']
-        created_password = request.form['password']
 
 
-        if (created_username not in users['username']) and (created_username.isalnum or created_username.isalpha):
-            database.users_collection.insertOne({'username': created_username, 'password': created_password})
-            session['username'] = created_username
 
-        else:
-            flash('Nome de usuário digitado já existe!')
+# Para existir uma outra função signup, deveria ter uma outra rota (@auth.route)
+# def sign_up():
+#     if request.method == "POST":
+#         created_username = request.form['username']
+#         created_password = request.form['password']
 
     
 
