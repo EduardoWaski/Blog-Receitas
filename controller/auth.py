87f15fa9d749c817b8b5c46d37933fc847d05afa
Eduardo Waski <eduardowaski688@gmail.com>
2024-09-04 21:41:11 -0300

    #18 primeira versao da autenticação do login

--- controller/auth.py
+++ controller/auth.py
@@ -1,7 +1,44 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, session, redirect, url_for, flash
+from models.db_config import *
 
 auth = Blueprint('auth', __name__)
 
-@auth.route("/acesso")
-def login_page():
-    return render_template("login_signup.html")
+# User fictício
+users = {
+    'username': 'eduardo', 'password': '1234'
+}
+
+@auth.route("/acesso", methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+
+        # Verificação dos dados
+        if (username in users['username']) and (users['password'] == password):
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            flash('Nome de usuário e/ou senha incorretos!')
+
+    return render_template('login_signup.html')
+
+def sign_up():
+    if request.method == "POST":
+        created_username = request.form['username']
+        created_password = request.form['password']
+
+
+        if (created_username not in users['username']) and (created_username.isalnum or created_username.isalpha):
+            database.users_collection.insertOne({'username': created_username, 'password': created_password})
+            session['username'] = created_username
+
+        else:
+            flash('Nome de usuário digitado já existe!')
+
+    
+
+
+
+
+
